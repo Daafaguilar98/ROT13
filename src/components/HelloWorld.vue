8f52b8bab39d0ace7049c6e3c27b6ac2f8578113
Diego Aguilar <daafaguilar98@gmail.com>
2020-05-08 18:14:51 -0500

    new release

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,154 +1,150 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha"
-          target="_blank"
-          rel="noopener"
-          >unit-mocha</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress"
-          target="_blank"
-          rel="noopener"
-          >e2e-cypress</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="matrix">
+    <div class="matrix__input">
+      <p class="matrix__input__label">Valiu as <strong>Matrix</strong></p>
+      <input type="text" placeholder="Enter encrypted text" v-model="encryptedText">
+      <button @click="encodingText()">DECODE</button>
+    </div>
+    <div class="matrix__output">
+      <p class="matrix__output__text">{{ newText }}</p>
+      <canvas></canvas>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+<script>
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+export default {
+  mounted() {
+    // Initialising the canvas
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+
+    // Setting the width and height of the canvas
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    // Setting up the letters
+    let letters = this.letters;
+    letters = letters.split('');
+
+  // Setting up the columns
+  const fontSize = 10;
+  const columns = canvas.width / fontSize;
+
+  // Setting up the drops
+  const drops = [];
+  for (let i = 0; i < columns; i++) {
+    drops[i] = 1;
+  }
+
+    // Setting up the draw function
+    function draw() {
+      ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      for (let i = 0; i < drops.length; i++) {
+        const text = letters[Math.floor(Math.random() * letters.length)];
+        ctx.fillStyle = '#0f0';
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+        drops[i]++;
+      }
+    }
+
+    // Loop the animation
+    setInterval(draw, 50);
+  },
+  data() {
+    return {
+      showCanvas: true,
+      encryptedText: "",
+      newText: "",
+      letters: "ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ"
+    }
+  },
+  watch: {
+    encryptedText(newValue) {
+      this.newText = newValue
+    },
+  },
+  methods: {
+    async encodingText() {
+      const letters = this.letters.toLowerCase().split('');
+
+      for(let i = 0; i < this.encryptedText.length; i++) {
+          const lastCharacter = this.newText[i].toLowerCase();
+          const text = this.newText.split("");
+          for(let i2 = 0; i2 < 6; i2++) {
+            text[i] = letters[Math.floor(Math.random() * letters.length)];
+            this.newText = text.join("");
+            await this.sleep(100)
+          }
+          text[i] = this.rationCharacterNtimes(lastCharacter, 13);
+            this.newText = text.join("");
+      }
+    },
+    rationCharacterNtimes(c, n) {
+      return String.fromCharCode((c.charCodeAt(0) + n - 97) % 26 + 97)
+    },
+    sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.matrix__text{
+  color: #00FF41;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.matrix__input{
+  padding: 20px;
+  height: 20vh;
+
+  &__label{
+    font-size: 22px;
+    margin-bottom: 20px;
+
+    strong {
+      color: #00FF41;
+    }
+  }
+
+  input{
+    background-color: rgba(255,255,255,0.2);
+    padding: 10px 15px;
+    font-size: 22px;
+    border-radius: 4px;
+    border: 1px solid #fff;
+    outline-color: #00FF41;
+    color: #fff;
+    margin-right: 5px;
+  }
+
+  button{
+    padding: 10px 15px;
+    font-size: 22px;
+    border-radius: 4px;
+    outline: none;
+    border-style: none;
+    cursor: pointer;
+  }
+
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.matrix__output{
+  width: 100%;
+  height: 80vh;
+  position: relative;
+  display: flex;
+  justify-content: center;
+
+  &__text{
+    position: absolute;
+    font-size: 33px;
+  }
 }
-a {
-  color: #42b983;
+.matrix__output canvas{
+  width: 100%;
+  height: 99%;
 }
 </style>
